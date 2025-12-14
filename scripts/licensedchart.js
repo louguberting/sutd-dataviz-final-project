2d970257a0c00eba9a30258a79f9eb35a9e18809
Lou Guberti Ng <louguberti@gmail.com>
2025-12-14 14:59:48 +0800

    Adda legend to the chart

--- scripts/licensedchart.js
+++ scripts/licensedchart.js
@@ -24,20 +24,24 @@ console.log(sumstat)
 
 // Stack the data: each group will be represented on top of each other
 const mygroups = ["Original", "Licensed"] // list of group names
-const mygroup = [0, 1] // list of group names
-const stackedData = d3.stack()
-    .keys(mygroup)
-    .value(function (d, key) {
-        
-        return d[1][key]?.num ? d[1][key]?.num : 0
-    })
-    (sumstat)
-
-
+// const mygroup = [0, 1] // list of group names
 // const stackedData = d3.stack()
-//     .keys(d3.union(data.map(d => d.group))) 
-//     .value(([, group], key) => group.get(key)?.num)
-//   (d3.index(data, d => d.year, d => d.group));
+//     .keys(mygroup)
+//     .value(function (d, key) {
+//         if(d[1][key]?.num) {
+//             return d[1][key].num
+//         }
+//         else{
+//             return d[1][key].num ? d[1][key]?.num : 0
+//         }
+//     })
+//     (sumstat)
+
+
+const stackedData = d3.stack()
+    .keys(d3.union(data.map(d => d.group)))
+    .value(([, group], key) => group.get(key)?.num ?? 0)
+    (d3.index(data, d => d.year, d => d.group));
 
 // Add X axis --> it is a date format
 const x = d3.scaleLinear()
@@ -45,26 +49,33 @@ const x = d3.scaleLinear()
     .range([0, width]);
 svg.append("g")
     .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x).ticks(5));
+    .call(d3.axisBottom(x).ticks(10).tickFormat(d3.format("d")));
+svg.append("text")
+    .attr("class", "axis-label")
+    .attr("x", width / 2)
+    .attr("y", height + margin.bottom)
+    .text("Year");
 
 // Add Y axis
 const y = d3.scaleLinear()
-    .domain([0, d3.max(data, function (d) { return +d.num; }) * 1.2])
+    .domain([0, d3.max(data, function (d) { return +d.num; }) * 1.5])
     .range([height, 0]);
 svg.append("g")
     .call(d3.axisLeft(y));
+svg.append("text")
+    .attr("class", "axis-label")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -(height / 2))
+    .attr("y", -margin.left + 20)
+    .text("Number of Sets");
 
-// color palette
-const color = d3.scaleOrdinal()
-    .domain(mygroups)
-    .range(['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999'])
-
+console.log(stackedData)
 // Show the areas
 svg
     .selectAll("mylayers")
     .data(stackedData)
     .join("path")
-    .style("fill", function (d) { name = mygroups[d.key - 1]; return color(name); })
+    .style("fill", function (d, i) { return i == 0 ? '#377eb8' : '#4daf4a' })
     .attr("d", d3.area()
         .x(function (d, i) { return x(d.data[0]); })
         .y0(function (d) { return y(d[0]); })
